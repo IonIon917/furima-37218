672a5c55d34c28822dd6ea548732162542cc1b35
IonIon917 <pmmadjt@gmail.com>
2022-04-13 18:40:37 +0900

    出品商品の保存と保存後の画面遷移

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -5,4 +5,16 @@ class ItemsController < ApplicationController
   def new
     @item = Item.new
   end
+
+  def create
+    Item.create(item_params)
+    if Item.create
+      redirect_to root_path
+    end
+  end
+
+  private
+  def item_params
+    params.require(:item).permit(:introduction, :item_name, :category_id, :status_id, :ship_from_id, :item_value, :delivery_charge_id, :days_to_ship_id ).merge(user_id: current_user.id)
+  end
 end
