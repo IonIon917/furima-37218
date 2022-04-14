43c6359f5534d3739941c4f76c41e059b5ed7d22
IonIon917 <pmmadjt@gmail.com>
2022-04-14 15:09:54 +0900

    imageカラムのバリデーションの追記

--- app/models/item.rb
+++ app/models/item.rb
@@ -5,6 +5,7 @@ class Item < ApplicationRecord
   belongs_to :delivery_charge
   belongs_to :ship_from
   belongs_to :status
+  has_one_attached :image
 
   validates :category_id, numericality: { other_than: 1 , message: "can't be blank"} 
   validates :status_id, numericality: { other_than: 1 , message: "can't be blank"} 
@@ -14,5 +15,5 @@ class Item < ApplicationRecord
   validates :introduction, presence: true
   validates :item_name, presence: true
   validates :item_value, presence: true, numericality: { in: 300..9999999 }
-
+  validates :image, presence: true
 end
