05f054a10875bcc03f2794f39af7d8b23e3de7b1
IonIon917 <pmmadjt@gmail.com>
2022-04-14 22:54:16 +0900

    itemモデルテストコード

--- app/models/item.rb
+++ app/models/item.rb
@@ -6,6 +6,7 @@ class Item < ApplicationRecord
   belongs_to :ship_from
   belongs_to :status
   has_one_attached :image
+  belongs_to :user
 
   validates :category_id, numericality: { other_than: 1 , message: "can't be blank"} 
   validates :status_id, numericality: { other_than: 1 , message: "can't be blank"} 
@@ -14,6 +15,6 @@ class Item < ApplicationRecord
   validates :days_to_ship_id, numericality: { other_than: 1 , message: "can't be blank"} 
   validates :introduction, presence: true
   validates :item_name, presence: true
-  validates :item_value, presence: true, numericality: { in: 300..9999999 }
+  validates :item_value, presence: true, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999 }
   validates :image, presence: true
 end
--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,5 +1,19 @@
 FactoryBot.define do
   factory :item do
-    
+    introduction            {Faker::Lorem.sentence}
+    item_name               {Faker::Lorem.words}
+    item_value              {Faker::Number.between(from: 300, to: 9999999)}
+    category_id             {Faker::Number.between(from: 2, to: 11)}
+    status_id               {Faker::Number.between(from: 2, to: 7)}
+    delivery_charge_id      {Faker::Number.between(from: 2, to: 3)}
+    ship_from_id            {Faker::Number.between(from: 1, to: 47)}
+    days_to_ship_id         {Faker::Number.between(from: 2, to: 4)}
+
+    association :user
+
+    after(:build) do |message|
+      message.image.attach(io: File.open('public/images/test_image.png'), filename: 'test_image.png')
+    end
+
   end
 end
