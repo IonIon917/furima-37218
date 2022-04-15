0c4c7fc70d36db02e3391203e94ca0a5b6d87483
IonIon917 <pmmadjt@gmail.com>
2022-04-15 17:26:35 +0900

    paramsの追記

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -19,6 +19,6 @@ class ItemsController < ApplicationController
 
   private
   def item_params
-    params.require(:item).permit(:introduction, :item_name, :category_id, :status_id, :ship_from_id, :item_value, :delivery_charge_id, :days_to_ship_id ).merge(user_id: current_user.id)
+    params.require(:item).permit(:introduction, :item_name, :category_id, :status_id, :ship_from_id, :item_value, :delivery_charge_id, :days_to_ship_id, :image ).merge(user_id: current_user.id)
   end
 end
